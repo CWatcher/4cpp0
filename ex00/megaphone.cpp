252e3f490d032d187e789eec497826f3dd9cc79f
Carmine Watcher <cwatcher@student.21-school.r>
2021-10-13 21:01:24 +0300

    [done] ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
 	for (int i = 1; i < argc; i++) {
 		for (int j = 0; argv[i][j]; j++)
 			std::cout << (char)toupper(argv[i][j]);
-		std:: cout << ' ';
 	}
 	std::cout << std::endl;
 }
--- ex00/t.sh
+++ ex00/t.sh
@@ -1,6 +1,31 @@
 #!/bin/zsh
 
+# $>./megaphone "shhhhh... I think the students are asleep..."
+# SHHHHH... I THINK THE STUDENTS ARE ASLEEP...
+# $>./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+# DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.
+# $>./megaphone
+# * LOUD AND UNBEARABLE FEEDBACK NOISE *
+# $>
+
+make > /dev/null && echo make ok!
+
 ARG='shhhhh... I think the students are asleep...'
 #ARG='a'
+diff <(echo '* LOUD AND UNBEARABLE FEEDBACK NOISE *') \
+     <(./megaphone)                                   \
+	|  cat -e \
+	&& [ ${pipestatus[1]} -eq 0 ] && echo 'ok!'
+
 diff <(echo ${ARG:u}) <(./megaphone $ARG) | cat -e \
+	&& [ ${pipestatus[1]} -eq 0 ] && echo 'ok!'
+
+diff <(echo 'SHHHHH... I THINK THE STUDENTS ARE ASLEEP...')        \
+     <(./megaphone "shhhhh... I think the students are asleep...") \
+	|  cat -e \
+	&& [ ${pipestatus[1]} -eq 0 ] && echo 'ok!'
+
+diff <(echo 'DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.')            \
+     <(./megaphone Damnit " ! " "Sorry students, I thought this thing was off.") \
+	|  cat -e \
 	&& [ ${pipestatus[1]} -eq 0 ] && echo 'ok!'
