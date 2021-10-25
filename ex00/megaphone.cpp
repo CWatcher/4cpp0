87f5a31922a7a3e58b948de7d54da89a4c72adde
Carmine Watcher <cwatcher@student.21-school.r>
2021-10-25 14:54:39 +0300

    [rf] toupper() -> std::toupper()

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -7,6 +7,6 @@ int main(int, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (; *argv; argv++)
 		for (char *s = *argv; *s; s++)
-			std::cout << (char)toupper(*s);
+			std::cout << (char)std::toupper(*s);
 	std::cout << std::endl;
 }
